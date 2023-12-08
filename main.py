c20cb92cc9bcab821f6fbe549c7c79732469afa0
selvaiitm <96175511+selvaiitm@users.noreply.github.com>
2023-12-08 21:31:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,18 @@ st.title("stream lit app ")
 st.markdown("""
     <h1 style='text-align: center;'>Centered Text</h1>
     """, unsafe_allow_html=True)
+
+---
+st.markdown("""
+<h1 align="center">  STREAMLIT APP </h1>
+""", unsafe_allow_html=True)
+
+---
+
+## THIS IS AN VERY BASIC LEVEL APP CREATED AS PART OF WEEKLY ASSIGNMENT FOR THE BS IN DATA SCIENCE COURSE OFFERED BY IIT MADRAS FOR THE SUBJECT NAMED TOOLS IN DATA SCIENCE 
+---
+<h3 align="center"> WEEK - 8 GRADED ASSIGNMENT </h3>
+<h3 align="center"> AN APP TO FIND THE LARGEST OF THE THREE GIVEN NUMBERS </h3>
+
+-----
+
