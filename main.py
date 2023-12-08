820f170d64d693bc05ae7b5a474ce493c6373eac
selvaiitm <96175511+selvaiitm@users.noreply.github.com>
2023-12-08 21:35:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,10 +12,21 @@ st.markdown("""
 
 st.markdown("<hr>", unsafe_allow_html=True)
 
-## THIS IS AN VERY BASIC LEVEL APP CREATED AS PART OF WEEKLY ASSIGNMENT FOR THE BS IN DATA SCIENCE COURSE OFFERED BY IIT MADRAS FOR THE SUBJECT NAMED TOOLS IN DATA SCIENCE 
+st.markdown("""
+<h3 align="center"> THIS IS AN VERY BASIC LEVEL APP CREATED AS PART OF WEEKLY ASSIGNMENT FOR THE BS IN DATA SCIENCE COURSE OFFERED BY IIT MADRAS FOR THE SUBJECT NAMED TOOLS IN DATA SCIENCE 
+""", unsafe_allow_html=True)
+
+
+
 st.markdown("<hr>", unsafe_allow_html=True)
+
+st.markdown("""
 <h3 align="center"> WEEK - 8 GRADED ASSIGNMENT </h3>
+""", unsafe_allow_html=True)
+
+st.markdown("""
 <h3 align="center"> AN APP TO FIND THE LARGEST OF THE THREE GIVEN NUMBERS </h3>
+""", unsafe_allow_html=True)
 
 st.markdown("<hr>", unsafe_allow_html=True)
 
